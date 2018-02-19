da19ed458b569f1e399e60888ca9d25191a33686
JessicaMP <jessica95.mp@gmail.com>
2018-02-19 15:41:46 -0500

    Funcionalidad de image completa

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,7 @@
     $('.modal').modal();
 })();
 
+//Function Time
 const timeDate = () => {
     let f = new Date();
     let time = f.getHours() + ":" + f.getMinutes();
@@ -16,15 +17,41 @@ const timeDate = () => {
     return timeAbsolute;
 }
 
-// Post de texto
+// Post de text
 function post() {
     const postEstructure = `<div class="styleBox">
     <div class="title"><h5 class="center-align">${$('.post-header input').val()}</h5></div>
     <div class="styleLetter"><p class="styleLetterP">${$('#texto').val()}</p></div>
-    <div>
-    <div class="left-align col s5"><span class="styleLetter">${timeDate()}</span></div></div>
+    <div class="right-align"><span class="styleLetter">${timeDate()}</span></div>
     </div>`;
     $('#post').prepend(postEstructure);
+    $('.post-header input').val(' ');
+    $('#texto').val(' ');
 }
 
 $('#btn-post').on('click', post);
+
+//Preview Image
+$('input[type=file]').change(function () {
+    let file = (this.files[0].name).toString();
+    let reader = new FileReader();
+
+    reader.onload = function (e) {
+        $('#modal2 img').attr('src', e.target.result);
+    }
+    reader.readAsDataURL(this.files[0]);
+});
+
+// Post de Image
+function image() {
+    let imagen = $("#img-guardar");
+
+    const imgEstructure = `<div class="styleBox">
+    <img src="${imagen[0].src}" class="post-img"></img>
+    <div class="styleLetter"><p class="styleLetterP">${$('#texto').val()}</p></div>
+    <div class="right-align"><span class="styleLetter">${timeDate()}</span></div>
+    </div>`;
+    $('#post').prepend(imgEstructure);
+}
+
+$('#btn-img').on('click', image);
