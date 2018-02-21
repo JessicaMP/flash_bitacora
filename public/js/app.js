efb5d0ff29a9321a92e4438204747e3d8751a441
mirelatv <noelia.mirela@gmail.com>
2018-02-20 23:49:42 -0500

    subiendo post de date

--- public/js/app.js
+++ public/js/app.js
@@ -105,3 +105,81 @@ $('#img-modal').on('click', function () {
     $('input[type=file]').change(previewImage);
     $('#btn-img').on('click', image);
 });
+//Post de Date
+function date() {
+
+    function success(position) {
+        let pos = {
+            lat: position.coords.latitude,
+            long: position.coords.longitude
+        }
+
+        const dateEstructure = `<div class="styleBox z-depth-5 col l5 s10 m6 offset-s1">
+        <div class="title"><h5 class="center-align">${$('#modal1 .post-header').val()}</h5></div>
+        <div class="styleLetter">
+        <p class="styleLetterP">${'Date of event: ' + $('input.datepicker').val()}</p>
+        <p class="styleLetterP">${'Hour: ' + $('input.timepicker').val()}</p>
+        </div>
+        <div><img class="col l12 s12" src="${'http://maps.googleapis.com/maps/api/staticmap?center=' + pos.lat + ',' + pos.long + '&zoom=13&size=300x300&sensor=false'}"></div>
+        <div class="right-align"><span class="styleLetter">${timeDate()}</span></div>
+        </div>`;
+        $('#post').prepend(dateEstructure);
+        $('#modal3 .post-header').val(' ');
+        $('input.datepicker').val(' ');
+        $('input.timepicker').val(' ');
+    };
+
+    function error() {
+        alert('Tenemos un problema con la ubicación');
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error);
+}
+
+//Modal date 
+$('#date-modal').on('click', function () {
+    $('#date-modal').addClass('waves-effect waves-light modal-trigger');
+    $('.modal').addClass('height');
+
+    const modalDateContent = `<div class="row">
+                                    <div class="col offset-l4 offset-s2">
+                                            <input type="text" name="" value="" placeholder="Title of video and audio" class="title post-header" autofocus>
+                                        </div>
+                                <div class="col l10 s12 offset-l2">
+                                    <div class="col l5 s12">
+                                        <label for="input_text">Date</label>
+                                        <input type="text" class="datepicker">
+                                    </div>
+                                    <div class="col l5 s12">
+                                            <label for="input_text">Hours</label>
+                                        <input type="text" class="timepicker">
+                                    </div>
+                                </div>
+                            </div>`
+    const modalDateFooter = `<a id="btn-date" href="#!" class="modal-action modal-close waves-effect waves-green btn-flat teal darken-1 white-text">
+    <i class="material-icons right">send</i>Public</a>`
+
+    $('.modal-content').html(modalDateContent);
+    $('.modal-footer').html(modalDateFooter);
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year,
+        today: 'Today',
+        clear: 'Clear',
+        close: 'Ok',
+        closeOnSelect: false // Close upon selecting a date,
+    });
+
+    $('.timepicker').pickatime({
+        default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+        fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+        twelvehour: false, // Use AM/PM or 24-hour format
+        donetext: 'OK', // text for done-button
+        cleartext: 'Clear', // text for clear-button
+        canceltext: 'Cancel', // Text for cancel-button
+        autoclose: false, // automatic close timepicker
+        ampmclickable: true, // make AM PM clickable
+        aftershow: function () { } //Function for after opening timepicker
+    });
+    $('#btn-date').on('click', date);
+});
